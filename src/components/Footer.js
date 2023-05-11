f6286aeed9cafbf119ff34aa9a5b55bee4467b7d
muhammad423 <mi1933065@gmail.com>
2023-05-11 22:10:31 +0500

    new chnages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <Box mt='80px' bgcolor='black'>
       <Stack alignItems='center' justifyContent='center' px='40px' py='20px'>
-        <Typography color='white'>
+        <Typography color='white' textAlign='center'>
           Copyright ! 2023  Made By <span style={{ color: '#ff2625' }}>Muhammad Israr</span>
         </Typography>
       </Stack>
--- src/components/SimilarVideos.js
+++ src/components/SimilarVideos.js
@@ -6,13 +6,13 @@ import Loader from './Loader'
 const SimilarVideos = ({targetMuscles, equipmentData}) => {
   return (
     <Box sx={{mt: {lg:'100', xs: '0'}}}>
-       <Typography variant='h3' mb={5} ml={3}>
+       <Typography sx={{fontSize: {md:'26px', xs:'20px'}, fontWeight: 'bold'}} mb={5} ml={3}>
           Exercises that target same muscles groups
        </Typography>
        <Stack sx={{p:'2px', position: 'relative'}}>
         {targetMuscles.length ? <HorizontalScrollbar data={targetMuscles}/>: <Loader />}
        </Stack>
-       <Typography variant='h3' mb={5} ml={3}>
+       <Typography sx={{fontSize: {md:'26px', xs:'20px'}, fontWeight: 'bold'}} mb={5} ml={3}>
           Exercises that target same Equipment groups
        </Typography>
        <Stack sx={{p:'2px', position: 'relative'}}>
